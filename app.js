c2de78ecb3ae5f9c79c6c3b027ee2faa8b5acc45
Eldar Terzic <eldar.terzic@insum.se>
2022-12-13 10:08:09 +0100

    implemet post request

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
-    {id: 1, name: "Course 1"},
-    {id: 2, name: "Course 2"},
-    {id: 3, name: "Course 3"}
+    {id: 1, name: 'Course 1'},
+    {id: 2, name: 'Course 2'},
+    {id: 3, name: 'Course 3'}
 ]
 
 app.get('/', (req, res) => {
@@ -13,16 +15,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses',(req, res) => {
-    res.send([1,2,3,4,5])
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
     let course = courses.find(c => c.id === parseInt(req.params.id));
 
-    if (!course) res.status(404).send("Course with given ID was not found");
+    if (!course) res.status(404).send('Course with given ID was not found');
     res.send(course);
 })
 
+app.post('/api/courses', (req, res) => {
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+})
 
 const port = process.env.PORT || 3023;
 app.listen(port, ()=> console.log(`Listening on port ${port} ...`));
