f4e3cd7de80df738c084e066f684aeaddadaf485
Eldar Terzic <eldar.terzic@insum.se>
2022-12-13 10:28:51 +0100

    validate request with joi

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -19,14 +19,25 @@ app.get('/api/courses',(req, res) => {
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
+    const course = courses.find(c => c.id === parseInt(req.params.id));
 
     if (!course) res.status(404).send('Course with given ID was not found');
     res.send(course);
 })
 
 app.post('/api/courses', (req, res) => {
-    let course = {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    const result = schema.validate(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error);
+        return;
+    }
+  
+    const course = {
         id: courses.length + 1,
         name: req.body.name
     };
