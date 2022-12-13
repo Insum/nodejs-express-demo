f2af6c4c60e9cffb6a072d4e5109269386f8e975
Eldar Terzic <eldar.terzic@insum.se>
2022-12-13 09:07:11 +0100

    get specific course

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+const courses = [
+    {id: 1, name: "Course 1"},
+    {id: 2, name: "Course 2"},
+    {id: 3, name: "Course 3"}
+]
+
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/api/courses',(req, res)=>{
+app.get('/api/courses',(req, res) => {
     res.send([1,2,3,4,5])
 });
+
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course) res.status(404).send("Course with given ID was not found");
+    res.send(course);
+})
 
 
 const port = process.env.PORT || 3023;
